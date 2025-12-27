33f539a2106f58677b1088f80f46412a4178b264
Vineet Waman <wamanvinit8717@gmail.com>
2025-12-27 22:25:58 +0530

    callback url fixed

--- Backend/config/passport.js
+++ Backend/config/passport.js
@@ -16,7 +16,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://findora-backend-pu0l.onrender.com/auth/google/callback",
       passReqToCallback: true,
     },
     async (req, accessToken, refreshToken, profile, done) => {
